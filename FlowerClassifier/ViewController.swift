87fe24b0b8456a7ff48cf639c48e28af322222ac
DK <dkhlv@users.noreply.github.com>
2019-06-30 09:59:07 +0300

    Set up image classifier

--- FlowerClassifier/ViewController.swift
+++ FlowerClassifier/ViewController.swift
@@ -7,14 +7,61 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var pickedImage : UIImage?
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+//        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
     }
 
-
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageView.image = userPickedImage
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            pickedImage = userPickedImage
+            detect(flowerImage : ciImage)
+            
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(flowerImage : CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Loading CoreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model){ (request, error) in
+            guard let classification = request.results?.first as? VNClassificationObservation else {
+                fatalError("Model failed to process image")
+            }
+         
+            self.navigationItem.title = classification.identifier.capitalized
+        }
+        
+        
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Error")
+        }
+    }
+    
 }
 
